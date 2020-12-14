c4aed4be65bd77042b7be0c6935ba4fd19f5763e
Travis Martin <Travis Martin@DESKTOP-GSTL8RO>
2020-12-14 16:08:34 -0500

    Hello World demo

---
+++ HelloWorldDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorldDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a word.");
+            string response = Console.ReadLine();
+            Console.WriteLine("Your reponse is " + response);
+        }
+    }
+}
